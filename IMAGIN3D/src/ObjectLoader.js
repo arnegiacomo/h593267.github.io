a7aafd0b93352031d374bdfbe24e3ab76bf1638b
Arne Munthe-Kaas <45269747+h593267@users.noreply.github.com>
2022-11-16 12:43:52 +0100

    fix paths

--- IMAGIN3D/src/ObjectLoader.js
+++ IMAGIN3D/src/ObjectLoader.js
@@ -1,161 +1,143 @@
-"use strict"
+"use strict";
 
 import * as THREE from "../libs/three.module.js";
-import {OBJLoader} from "../libs/OBJLoader.js";
+import { OBJLoader } from "../libs/OBJLoader.js";
 
 export default class ObjectLoader {
+  // Class that loads all objects from models to scene
+  init(terrain) {
+    const loader = new OBJLoader();
 
-    // Class that loads all objects from models to scene
-    init(terrain) {
+    const trees = new Array(4);
+    // load a resource
+    loader.load(
+      // resource URL
+      "../IMAGIN3D/assets/models/tree.obj",
+      // called when resource is loaded
+      function (object) {
+        createTree(object, 0.5, 3.5, 0);
+        terrain.add(object);
+        trees[0] = object.mesh;
+      },
+      // called when loading is in progresses
+      function (xhr) {
+        console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+      },
+      // called when loading has errors
+      function (error) {
+        console.log("An error happened" + error);
+      }
+    );
 
-        const loader = new OBJLoader();
+    loader.load(
+      // resource URL
+      "../IMAGIN3D/assets/models/tree.obj",
+      // called when resource is loaded
+      function (object) {
+        createTree(object, 5, 4.8, 5);
+        object.rotateY((Math.PI / 180) * 210);
+        terrain.add(object);
+        trees[1] = object.mesh;
+      },
+      // called when loading is in progresses
+      function (xhr) {
+        console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+      },
+      // called when loading has errors
+      function (error) {
+        console.log("An error happened" + error);
+      }
+    );
 
-        const trees = new Array(4);
-        // load a resource
-        loader.load(
-            // resource URL
-            '../assets/models/tree.obj',
-            // called when resource is loaded
-            function (object) {
+    loader.load(
+      // resource URL
+      "../IMAGIN3D/assets/models/tree.obj",
+      // called when resource is loaded
+      function (object) {
+        createTree(object, -7, 2.7, -0.45);
+        object.rotateY((Math.PI / 180) * 45);
+        terrain.add(object);
+        trees[2] = object.mesh;
+      },
+      // called when loading is in progresses
+      function (xhr) {
+        console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+      },
+      // called when loading has errors
+      function (error) {
+        console.log("An error happened" + error);
+      }
+    );
 
-                createTree(object,0.5, 3.5, 0);
-                terrain.add(object);
-                trees[0] = object.mesh;
+    loader.load(
+      // resource URL
+      "../IMAGIN3D/assets/models/tree.obj",
+      // called when resource is loaded
+      function (object) {
+        createTree(object, 5.7, 4.3, -4.75);
+        object.rotateY((Math.PI / 180) * 90);
+        terrain.add(object);
+        trees[3] = object.mesh;
+      },
+      // called when loading is in progresses
+      function (xhr) {
+        console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+      },
+      // called when loading has errors
+      function (error) {
+        console.log("An error happened" + error);
+      }
+    );
 
-            },
-            // called when loading is in progresses
-            function (xhr) {
-
-                console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-
-            },
-            // called when loading has errors
-            function (error) {
-
-                console.log('An error happened' + error);
-
-            }
-        );
-
-        loader.load(
-            // resource URL
-            '../assets/models/tree.obj',
-            // called when resource is loaded
-            function (object) {
-
-                createTree(object,5, 4.8, 5);
-                object.rotateY((Math.PI / 180) * 210);
-                terrain.add(object);
-                trees[1] = object.mesh;
-
-            },
-            // called when loading is in progresses
-            function (xhr) {
-
-                console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-
-            },
-            // called when loading has errors
-            function (error) {
-
-                console.log('An error happened' + error);
-
-            }
-        );
-
-        loader.load(
-            // resource URL
-            '../assets/models/tree.obj',
-            // called when resource is loaded
-            function (object) {
-
-                createTree(object,-7, 2.7, -0.45);
-                object.rotateY((Math.PI / 180) * 45);
-                terrain.add(object);
-                trees[2] = object.mesh;
-
-            },
-            // called when loading is in progresses
-            function (xhr) {
-
-                console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-
-            },
-            // called when loading has errors
-            function (error) {
-
-                console.log('An error happened' + error);
-
-            }
-        );
-
-        loader.load(
-            // resource URL
-            '../assets/models/tree.obj',
-            // called when resource is loaded
-            function (object) {
-
-                createTree(object, 5.7, 4.3, -4.75);
-                object.rotateY((Math.PI / 180) * 90);
-                terrain.add(object);
-                trees[3] = object.mesh;
-
-            },
-            // called when loading is in progresses
-            function (xhr) {
-
-                console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-
-            },
-            // called when loading has errors
-            function (error) {
-
-                console.log('An error happened' + error);
-
-            }
-        );
-
-        return trees;
-    }
+    return trees;
+  }
 }
 
-const spritemap = new THREE.TextureLoader().load( '../assets/images/canopy.png');
-const spritematerial = new THREE.SpriteMaterial( { map: spritemap, color: 0xffffff } );
+const spritemap = new THREE.TextureLoader().load(
+  "../IMAGIN3D/assets/images/canopy.png"
+);
+const spritematerial = new THREE.SpriteMaterial({
+  map: spritemap,
+  color: 0xffffff,
+});
 
 function createTree(object, x, y, z) {
-    object.mesh.material =  new THREE.MeshPhongMaterial( { color: THREE.Color.NAMES.sandybrown, side: THREE.DoubleSide } );
-    object.position.set(x, y, z);
-    object.scale.set(1.5, 1.5, 1.5);
-    object.mesh.castShadow = true;
-    object.mesh.receiveShadow = false;
-    object.mesh.isTree = true;
+  object.mesh.material = new THREE.MeshPhongMaterial({
+    color: THREE.Color.NAMES.sandybrown,
+    side: THREE.DoubleSide,
+  });
+  object.position.set(x, y, z);
+  object.scale.set(1.5, 1.5, 1.5);
+  object.mesh.castShadow = true;
+  object.mesh.receiveShadow = false;
+  object.mesh.isTree = true;
 
-    let sprite = new THREE.Sprite( spritematerial );
-    sprite.translateY(0.9);
-    sprite.scale.set(0.75, 0.75, 0.75)
-    object.mesh.add( sprite );
+  let sprite = new THREE.Sprite(spritematerial);
+  sprite.translateY(0.9);
+  sprite.scale.set(0.75, 0.75, 0.75);
+  object.mesh.add(sprite);
 
-    sprite = new THREE.Sprite( spritematerial );
-    sprite.translateY(0.75);
-    sprite.translateZ(0.4)
-    sprite.scale.set(0.75, 0.75, 0.75)
-    object.mesh.add( sprite );
+  sprite = new THREE.Sprite(spritematerial);
+  sprite.translateY(0.75);
+  sprite.translateZ(0.4);
+  sprite.scale.set(0.75, 0.75, 0.75);
+  object.mesh.add(sprite);
 
-    sprite = new THREE.Sprite( spritematerial );
-    sprite.translateY(0.75);
-    sprite.translateZ(-0.4)
-    sprite.scale.set(0.75, 0.75, 0.75)
-    object.mesh.add( sprite );
+  sprite = new THREE.Sprite(spritematerial);
+  sprite.translateY(0.75);
+  sprite.translateZ(-0.4);
+  sprite.scale.set(0.75, 0.75, 0.75);
+  object.mesh.add(sprite);
 
-    sprite = new THREE.Sprite( spritematerial );
-    sprite.translateY(0.75);
-    sprite.translateX(0.6)
-    sprite.scale.set(0.75, 0.75, 0.75)
-    object.mesh.add( sprite );
+  sprite = new THREE.Sprite(spritematerial);
+  sprite.translateY(0.75);
+  sprite.translateX(0.6);
+  sprite.scale.set(0.75, 0.75, 0.75);
+  object.mesh.add(sprite);
 
-    sprite = new THREE.Sprite( spritematerial );
-    sprite.translateY(0.75);
-    sprite.translateX(-0.4)
-    sprite.scale.set(0.75, 0.75, 0.75)
-    object.mesh.add( sprite );
+  sprite = new THREE.Sprite(spritematerial);
+  sprite.translateY(0.75);
+  sprite.translateX(-0.4);
+  sprite.scale.set(0.75, 0.75, 0.75);
+  object.mesh.add(sprite);
 }
--- IMAGIN3D/src/scene/water/Ocean.js
+++ IMAGIN3D/src/scene/water/Ocean.js
@@ -1,44 +1,36 @@
 "use strict";
 
 import * as THREE from "../../../libs/three.module.js";
-import { Water } from '../../../libs/Water.js';
-
-
+import { Water } from "../../../libs/Water.js";
 
 export default class Ocean {
+  constructor(scene) {
+    const waterGeometry = new THREE.PlaneGeometry(10000, 10000);
 
-    constructor(scene) {
-        const waterGeometry = new THREE.PlaneGeometry( 10000, 10000 );
+    this.water = new Water(waterGeometry, {
+      textureWidth: 512,
+      textureHeight: 512,
+      waterNormals: new THREE.TextureLoader().load(
+        "../../../IMAGIN3D/assets/images/waternormals.jpg",
+        function (texture) {
+          texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+        }
+      ),
+      sunDirection: new THREE.Vector3(),
+      sunColor: 0xffffff,
+      waterColor: 0x001e0f,
+      distortionScale: 3.7,
+      fog: scene.fog !== undefined,
+    });
+    this.water.isOcean = true;
 
-        this.water = new Water(
-            waterGeometry,
-            {
-                textureWidth: 512,
-                textureHeight: 512,
-                waterNormals: new THREE.TextureLoader().load( '../../../assets/images/waternormals.jpg', function ( texture ) {
+    this.water.rotation.x = -Math.PI / 2;
+    this.water.position.y += 0.1;
 
-                    texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+    scene.add(this.water);
+  }
 
-                } ),
-                sunDirection: new THREE.Vector3(),
-                sunColor: 0xffffff,
-                waterColor: 0x001e0f,
-                distortionScale: 3.7,
-                fog: scene.fog !== undefined
-            }
-        );
-        this.water.isOcean = true;
-
-        this.water.rotation.x = - Math.PI / 2;
-        this.water.position.y += 0.1;
-
-        scene.add( this.water );
-    }
-
-    update(dt) {
-
-        this.water.material.uniforms[ 'time' ].value += dt / 5;
-
-    }
-
+  update(dt) {
+    this.water.material.uniforms["time"].value += dt / 5;
+  }
 }
